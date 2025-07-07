d9d2da39116817a2a4ea47e4d1c5f75a4f161253
Daniel Vega <dani.vega.aguirre@gmail.com>
2025-07-07 17:13:05 -0400

    Add Test: Service Name Cases

---
+++ Calculations.Test/NamesServiceTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculations.Test
+{
+    public class NamesServiceTest
+    {
+        [Fact]
+        public void MakeFullName_GivenFirstNameAndLastName_StartWithFirstName()
+        {
+            //arrange
+            var namesService = new NamesService();
+
+            //act
+            var res = namesService.MakeFullName("daniel","Vega");
+
+            //assert
+            Assert.StartsWith("Daniel Vega",res,StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void MakeFullName_GivenFirstNameAndLastName_ContainsFirstName()
+        {
+            //arrange
+            var namesService = new NamesService();
+
+            //act
+            var res = namesService.MakeFullName("daniel", "vega");
+
+            //assert
+            Assert.Contains("Daniel Vega",res,StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void MakeFullName_GivenFirstNameAndLastName_RegexTest()
+        {
+            //arrange
+            var namesService = new NamesService();
+
+            //act
+            var res = namesService.MakeFullName("Daniel", "Vega");
+
+            //assert
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", res);
+        }
+    }
+}
---
+++ Calculations/NamesService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculations
+{
+    public class NamesService
+    {
+        public string MakeFullName(string firstName, string lastName) => $"{firstName} {lastName}";
+    }
+}
